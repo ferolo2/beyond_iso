db0c4011e3df71ae3aee12b61de0385e0d9ccf92
blanton1 <33041587+blanton1@users.noreply.github.com>
2018-09-12 01:32:40 -0700

    Add files via upload

--- F3/sums_alt.py
+++ F3/sums_alt.py
@@ -7,7 +7,7 @@ from numpy.lib.scimath import sqrt
 
 
 #from pathlib import Path
-#from numba import jit
+from numba import jit, autojit
 from scipy.special import sph_harm
 from scipy.special import erfi
 from scipy.special import erfc
@@ -21,11 +21,39 @@ xmax = 0.97
 xmin = 0.01
 
 
+@jit(nopython=True,fastmath=True) #FRL, this speeds up like 5-10%
+def npsqrt(x):
+    return np.sqrt(x)
+ 
+@jit(nopython=True,fastmath=True) #FRL, this speeds up like 5-10%
+def square(x):
+    return x**2
+ 
+ 
+@jit(nopython=True,fastmath=True)
+def exp(x):
+    return np.exp(x)
+ 
+@jit(nopython=True,parallel=True,fastmath=True)
+def mydot(x,y):
+    res = 0.
+    for i in range(3):
+        res+=x[i]*y[i]
+    return res
+
+@jit(nopython=True, parallel=True,fastmath=True) #FRL this speeds up like 20%
+def norm(nnk):
+    nk=0.
+    for i in nnk:
+        nk += i**2
+    return npsqrt(nk)
+
+
 def jj( x):
     xmin = 0.01
     xmax = 0.97
     if xmin < x < xmax:
-        return np.exp(-np.exp(-1/(1-x))/x)
+        return exp(-exp(-1/(1-x))/x)
     elif x >= xmax:
         return 1.
     else:
@@ -33,13 +61,7 @@ def jj( x):
 
 # E2k**2 / 4
 def E2a2(e, a):
-    return (1.+e**2)/4. - e*np.sqrt(1.+a**2)/2
-
-def norm(nnk):
-    nk=0.
-    for i in nnk:
-        nk += i**2
-    return np.sqrt(nk)
+    return (1.+square(e))/4. - e*npsqrt(1.+square(a))/2
 
 def hh(e, k):
     alpH = -1.
@@ -48,44 +70,13 @@ def hh(e, k):
     return jj( (E2a2(e,k) - aux1)/aux2  )
 
 def gam(e, k):
-    return (e - np.sqrt(1. + k**2))/(2*np.sqrt(E2a2(e, k)))
+    return (e - npsqrt(1. + square(k)))/(2*npsqrt(E2a2(e, k)))
 
 def xx2(e, L, k):
-    return ( E2a2(e, k) - 1)*L*L/(2*math.pi)**2;
+    return ( E2a2(e, k) - 1)*L*L/square(2*math.pi);
 
 
-# def xx2_TB(E,L,k):
-#   return ( defns.qst(E,k)*L/(2*pi) )**2
 
-# def rr2_TB():
-
-
-# def summand(e, L, nna, nnk, gamma, x2,l1,m1,l2,m2,alpha):
-
-#     nk = norm(nnk);
-
-#     nnA = np.array(nna)
-#     nnK = np.array(nnk)
-#     nnb = -1*nnA -1*nnK
-    
-#     if(nk==0):
-#         rr=nnA
-#     else:
-#         rr = nnA + nnK/(2*gamma) + nnK*(1/gamma -1)*np.dot(nnA,nnK)/nk**2       
-
-#     rr2 = np.dot(rr, rr)
-#     twopibyL = 2*math.pi/L
-#     a = norm(nnA)*twopibyL
-#     b = norm(nnb)*twopibyL
-
-#     Theta = np.arctan2(np.sqrt(rr[1]**2+rr[0]**2),rr[2])
-#     Phi = np.arctan2(rr[1],rr[0])
-    
-#     Ylmlm =4*math.pi* sph_harm(m1,l1,Phi,Theta) * np.conj(sph_harm(m2,l2,Phi,Theta))*(np.sqrt(rr2))**(l1 + l2)
-    
-#     exponential = np.exp(alpha*(x2-rr2))
-    
-#     return Ylmlm*exponential/(x2 - rr2)
 
 # TB: choose basis inside
 def summand(e, L, nna, nnk, gamma, x2,l1,m1,l2,m2,alpha):
@@ -99,9 +90,9 @@ def summand(e, L, nna, nnk, gamma, x2,l1,m1,l2,m2,alpha):
     if(nk==0):
         rr=nnA
     else:
-        rr = nnA + nnK/(2*gamma) + nnK*(1/gamma -1)*np.dot(nnA,nnK)/nk**2       
+        rr = nnA + nnK/(2*gamma) + nnK*(1/gamma -1)*mydot(nnA,nnK)/square(nk)
 
-    rr2 = np.dot(rr, rr)
+    rr2 = mydot(rr, rr)
     twopibyL = 2*math.pi/L
     a = norm(nnA)*twopibyL
     b = norm(nnb)*twopibyL
@@ -114,7 +105,7 @@ def summand(e, L, nna, nnk, gamma, x2,l1,m1,l2,m2,alpha):
     if l2==2:
       Ylmlm = Ylmlm * defns.y2(rr,m2,Ytype)
     
-    exponential = np.exp(alpha*(x2-rr2))
+    exponential = exp(alpha*(x2-rr2))
     
     out = Ylmlm*exponential/(x2 - rr2)
     if (Ytype=='r' or Ytype=='real') and abs(out.imag)>1e-15:
@@ -124,9 +115,10 @@ def summand(e, L, nna, nnk, gamma, x2,l1,m1,l2,m2,alpha):
     return out
 
 
-# May edit to try improving run-time at shell thresholds
+
+# TB: May edit to try improving run-time at shell thresholds
 def getnmax(cutoff,alpha,x2,gamma):
-    eqn = lambda l : -cutoff + 2*math.pi*np.sqrt(math.pi/alpha) * np.exp(alpha*x2)*erfc(np.sqrt(alpha)*l)
+    eqn = lambda l : -cutoff + 2*math.pi*npsqrt(math.pi/alpha) * exp(alpha*x2)*erfc(npsqrt(alpha)*l)
 
     
     n0=1
@@ -143,7 +135,7 @@ def sum_nnk(e, L, nnk,l1,m1,l2,m2,alpha):
     else:
         twopibyL = 2.*math.pi/L
         k = nk*twopibyL
-        #nn0 = np.sqrt((e**2 - alpH)**2/(4*e**2) - 1)/twopibyL;
+        #nn0 = npsqrt((e**2 - alpH)**2/(4*e**2) - 1)/twopibyL;
         gamma = gam(e, k)
         x2 = xx2(e, L, k)
  #       nmax = math.floor(nn0);
@@ -158,8 +150,9 @@ def sum_nnk(e, L, nnk,l1,m1,l2,m2,alpha):
         for n1 in range(-nmax,nmax+1):
             for n2 in range(-nmax,nmax+1):
                 for n3 in range(-nmax,nmax+1):
-                    ressum += summand(e, L, [n1, n2, n3], nnk, gamma, x2,l1,m1,l2,m2,alpha) #TB
-                    #ressum += hhk*summand(e, L, [n1, n2, n3], nnk, gamma, x2,l1,m1,l2,m2,alpha)
+                    if norm([n1,n2,n3])<=nmax:
+                      ressum += summand(e, L, [n1, n2, n3], nnk, gamma, x2,l1,m1,l2,m2,alpha) #TB
+                      #ressum += hhk*summand(e, L, [n1, n2, n3], nnk, gamma, x2,l1,m1,l2,m2,alpha)
             
         # return (x2*twopibyL**2)**(-(l1+l2)/2)*ressum # FRL
         #return x2**(-(l1+l2)/2)*ressum # TB
@@ -180,7 +173,7 @@ def int_nnk(e,L,nnk,l1,m1,l2,m2,alpha):
         return 0.
 
     elif(l1==l2==0):
-        factor1 = -sqrt(math.pi/alpha)*0.5*np.exp(alpha*(x2))
+        factor1 = -sqrt(math.pi/alpha)*0.5*exp(alpha*(x2))
         factor2 = 0.5*math.pi*sqrt(x2)*erfi(sqrt(alpha*x2))
 
         #out = q2s*hhk*4*math.pi*gamma*(factor1 + factor2) # FRL
@@ -188,7 +181,7 @@ def int_nnk(e,L,nnk,l1,m1,l2,m2,alpha):
         out = 4*math.pi*gamma*(factor1 + factor2) #TB, no q
 
     elif(l1==l2==2):
-        factor1 = -np.sqrt(math.pi/alpha**5)*(3+2*alpha*x2+4*alpha**2*x2**2)*np.exp(alpha*(x2))/8
+        factor1 = -npsqrt(math.pi/alpha**5)*(3+2*alpha*x2+4*square(alpha*x2))*exp(alpha*(x2))/8
         factor2 = 0.5*math.pi*sqrt(x2**5)*erfi(sqrt(alpha*x2))
 
         #out = q2s*hhk*4*math.pi*gamma*(factor1 + factor2) # FRL
@@ -209,7 +202,7 @@ def int_nnk(e,L,nnk,l1,m1,l2,m2,alpha):
 def F2KSS(e,L,nnk,l1,m1,l2,m2,alpha):
     nk = norm(nnk)
     k = nk*2*math.pi/L
-    omk = np.sqrt(1. + k**2)
+    omk = npsqrt(1. + square(k))
     hhk = hh(e, k)
     
     if hhk==0:
@@ -217,7 +210,7 @@ def F2KSS(e,L,nnk,l1,m1,l2,m2,alpha):
     else:
       SUM = sum_nnk(e, L, nnk,l1,m1,l2,m2,alpha)
       INT = int_nnk(e,L,nnk,l1,m1,l2,m2,alpha)
-      C = hhk/(32*omk*math.pi**2*L*(e - omk)) #TB
+      C = hhk/(32*omk*square(math.pi)*L*(e - omk)) #TB
       #C = 1/(32*omk*math.pi**2*L*(e - omk))
       return (SUM-INT)*C
 
@@ -233,7 +226,7 @@ def sum_nnk_test(e, L, nnk,l1,m1,l2,m2,alpha,nmax2):
 
         twopibyL = 2.*math.pi/L
         k = nk*twopibyL
-        #nn0 = np.sqrt((e**2 - alpH)**2/(4*e**2) - 1)/twopibyL;
+        #nn0 = npsqrt((e**2 - alpH)**2/(4*e**2) - 1)/twopibyL;
         gamma = gam(e, k)
         x2 = xx2(e, L, k)
 #        nmax = math.floor(nn0);
@@ -251,6 +244,6 @@ def sum_nnk_test(e, L, nnk,l1,m1,l2,m2,alpha,nmax2):
 
 
                     
-        return (x2*twopibyL**2)**(-(l1+l2)/2)*ressum
+        return (x2*square(twopibyL))**(-(l1+l2)/2)*ressum
 
 
