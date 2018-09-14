dfff8cc83b1922a944193b44773b54a46bbc4d40
ferolo2 <fromerol@fromero.ific.uv.es>
2018-09-14 11:02:02 +0200

    include testing.py

--- testing.py
+++ testing.py
@@ -2,6 +2,7 @@ import os, sys, time, pickle
 cwd = os.getcwd()
 sys.path.insert(0,cwd+'/F3')
 from F3 import H_mat
+from F3 import sums_alt as sums
 from F3 import F2_alt as F2
 from F3 import K2i_mat as K2
 from F3 import Gmatrix as Gm
@@ -9,45 +10,60 @@ import defns
 import math
 import numpy as np
 import time 
-from numba import jit,njit,prange,autojit
+from numba import jit,njit,prange,autojit,vectorize
 from F3 import sums_alt as sums
-
-
-@autojit
-def detrange2(L,a,energies):
-    res=[0.,0.,0.]
-    lenergies= len(list(energies))
-
-    for i in prange(lenergies):        
-       res[i]= np.linalg.det(H_mat.Hmat(energies[i],L,a,0.,0.,1.0,0.5))        
-    return res
+from scipy.special import erfc,erfc
+from scipy.optimize import fsolve
+from joblib import Parallel, delayed
+npsqrt = np.sqrt
+exp = np.exp
 
 
 
+def getnmax(cutoff,alpha,x2,gamma):
+    eqn = lambda l : -cutoff + 2*math.pi*npsqrt(math.pi/alpha) * exp(alpha*x2)*erfc(npsqrt(alpha)*l)
 
 
+    n0=1
+    solution=fsolve(eqn, n0)
+
+    print(max(solution*gamma,1)+3)
+    
+    return int(np.round(max(solution*gamma,1)+3))
+
+    
 
 
-L=18
-E=3.0007
-nnk = [1,2,3]
+L=20
+E=3.01
+nnk = np.array([1,2,3])
 alpha=0.5
 
+gamma = sums.gam(E,npsqrt(1+4+9)*2*math.pi/L)
+x2= sums.xx2(E, L, npsqrt(1+4+9)*2*math.pi/L)
+
+cutoff=1e-9
 #Fmat_k(E,L,nnk,alpha)
 
 #Gt = Gmatrix.Gmat(E,L)
 
 
-start = time.time()
-#shell_nnk_list
-print(defns.shell_nnk_list(nnk))
-#print(F2.Fmat_shell(E,L,nnk,alpha))
-#print(np.shape(F2.Fmat_shell(E,L,nnk,alpha)))
 
-#print(sums.sum_nnk(E,L,nnk,2,0,2,0,alpha))
+
+
+start = time.time()
+
+
+Parallel(n_jobs=4)(delayed(sums.sum_nnk)(E, L,nnk,2,0,2,0,alpha) for i in range(1000))
 end = time.time()
 print('time is:', end - start, ' s')
 
+
+start = time.time()
+for i in range(1000):
+    sums.sum_nnk(E, L,nnk,2,0,2,0,alpha)
+end = time.time()
+print('time is:', end - start, ' s')
 
 
 
